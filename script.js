f49583379273582303aeab36210a2b1bdecb59ab
cristofer.domingues <cristofer.domingues@jtech.com>
2016-06-10 14:55:00 -0300

    isValid attribute added

--- script.js
+++ script.js
@@ -2,23 +2,74 @@ var angular = angular || undefined;
 
 angular.module('app', ['ionic'])
 
+.controller('appCtrl', function($scope, $timeout) {
+
+ $scope.objeto1 = {};
+ $scope.objeto  = {};
+
+  $scope.forms = {
+         allValid: function () {
+             var formValidCount = 1;
+             angular.forEach(this, function (obj) {
+                 if (obj.$valid) {
+                     formValidCount++;
+                 }
+             });
+             return (Object.keys(this).length === formValidCount);
+         }
+     };
+
+     $scope.setForm = function (form, id) {
+         var formObj = {};
+         $scope.$applyAsync(function () {
+             formObj[id] = form;
+             $scope.forms[id] = formObj[id][id];
+         });
+     };
+
+     $scope.validaForm = function(form) {
+		if($scope.forms[form]){
+			return $scope.forms[form].$valid;
+		}
+	};
+})
+
 .component('collapseContainer', {
   templateUrl: 'collapse.view.html',
-  transclude:true,
-  bindings: { 
-    title: '=',
-    leftHeaderIdentifier:'=',
-    headerIcon: '='
+  transclude: true,
+  bindings: {
+    title: '<',
+    leftHeaderIdentifier: '<',
+    headerIcon: '<',
+    isOpen: '=',
+    leftHeaderCss: '<',
+    headerCss: '<',
+    preventDoubleClick: '<',
+    isValid: '<'
+
   },
-  controller: function() {
+  controller: function($timeout) {
+
+    var _self = this,
+      _wasClicked,
+      _preventDoubleClick = function() {
+        if (_self.preventDoubleClick) {
+          _wasClicked = true;
+          $timeout(function() {
+            _wasClicked = false;
+          }, 1500);
+        }
+      };
 
     this.$onInit = function() {
-      this.showHideFlag = false;
+      this.showHideFlag = this.isOpen;
     };
 
     this.showHide = function() {
-      this.showHideFlag = !this.showHideFlag;
-    };
+      if (!_wasClicked)
+        this.showHideFlag = !this.showHideFlag;
+      _preventDoubleClick();
+    };    
   }
 })
 
